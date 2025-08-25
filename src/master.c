5c8a89422f5d9610b93a7b7bbce694b0091ae0d3
Jaime Nazar Anchorena <jnazaranchorena@itba.edu.ar>
2025-08-25 18:52:23 -0300

    chore: master

--- src/master.c
+++ src/master.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <sys/mman.h> 
+#include <sys/stat.h>       
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <semaphore.h>  
 
 // TODO: Put in common.h shared dependencies
 
@@ -20,6 +29,8 @@ typedef struct {
 	char view_path[MAX_STR_LEN];
 	char player_path[MAX_PLAYERS][MAX_STR_LEN];
 	int player_count;
+	GameState *game_state;
+	GameSync *game_sync;
 } MasterCDT;
 
 typedef MasterCDT* MasterADT;
@@ -106,6 +117,52 @@ static int parse_args(MasterADT m, int argc, char *argv[]) {
 	return 0;
 }
 
+static int init_sem(MasterADT m) {
+
+	return 0;
+}
+
+static int init_state(MasterADT m) {
+
+	return 0;
+}
+
+static int init_sync(MasterADT m) {
+	
+
+	// Create semaphores
+	sem_init(&m->game_sync->state_change, 1, 0);
+	//m->game_sync->render_done = *sem_open("/sem_render_done", O_CREAT, 0644, 0);	
+	//m->game_sync->master_mutex = *sem_open("/sem_master_mutex", O_CREAT, 0644, 0);	
+	//m->game_sync->state_mutex = *sem_open("/sem_state_mutex", O_CREAT, 0644, 0);	
+	//m->game_sync->reader_count_mutex = *sem_open("/sem_reader_count_mutex", O_CREAT, 0644, 0);
+	// Create the player movement sem with a unique name for each
+	//char player_sem[21] = { 0 };
+	//for (int i = 0; i < m->player_count; i++) {
+		// TODO: Se puede cambiar el string que le pase a sem_open?	
+	//	sprintf(player_sem, "/sem_player_can_move", i);
+	//	m->game_sync->player_can_move[i] = *sem_open(player_sem, O_CREAT, 0644, 0);	
+	//}
+
+	return 0;
+}
+
+static int init_shm(MasterADT m) {
+
+	// TODO: Error check, investigar flags, son las de ChompChamps sacadas con strace
+	// Game state
+	int fd = shm_open(GAME_STATE_SHM, O_CREAT | O_RDWR | O_NOFOLLOW | O_CLOEXEC, 0644);  
+	ftruncate(fd, sizeof(GameState));  
+	m->game_state = mmap(0, sizeof(GameState), PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+
+	// Game sync
+	fd = shm_open(GAME_SYNC_SHM, O_CREAT | O_RDWR | O_NOFOLLOW | O_CLOEXEC, 0666);  
+	ftruncate(fd, sizeof(GameSync));  
+	m->game_sync = mmap(0, sizeof(GameSync), PROT_READ | PROT_READ, MAP_SHARED, fd, 0);
+
+	return 0;
+}
+
 int main (int argc, char *argv[]) {
 
 	MasterCDT m = { DEFAULT_WIDTH,
@@ -122,7 +179,10 @@ int main (int argc, char *argv[]) {
 		return -1;
 	}
 
-
+	// Initialization
+	init_shm(&m);
+	init_state(&m);
+	init_sync(&m);
 
 	return 0;
 }
